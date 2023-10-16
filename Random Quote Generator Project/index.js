a483710222982264dae680812e4305c4fb753628
Dextrox007 <deshex007@gmail.com>
2023-10-16 02:02:54 +0100

    Download button working
    Git download to work now working on creating a search bar that searches the api for selected "quote themes"

--- Random Quote Generator Project/index.js
+++ Random Quote Generator Project/index.js
@@ -1,87 +1,93 @@
-const quoteText = document.querySelector(".quote"),
-  authorName = document.querySelector(".author .name"),
-  quoteBtn = document.querySelector(".banother"),
-  soundBtn = document.querySelector(".sound"),
-  copyBtn = document.querySelector(".copy"),
-  twitterBtn = document.querySelector(".twitter"),
-  searchBtn = document.querySelector(".bsearch"),
-  wrapper = document.querySelector('.wrapper'),
-  printBtn = document.querySelector(".printScrn");
+class RandomQuoteGenerator {
+  constructor() {
+    this.quoteText = document.querySelector(".quote");
+    this.authorName = document.querySelector(".author .name");
+    this.quoteBtn = document.querySelector(".banother");
+    this.soundBtn = document.querySelector(".sound");
+    this.copyBtn = document.querySelector(".copy");
+    this.twitterBtn = document.querySelector(".twitter");
+    this.searchBtn = document.querySelector(".bsearch");
+    this.wrapper = document.querySelector('.wrapper');
+    this.printBtn = document.querySelector(".printScrn");
+    this.downloadBtn = document.querySelector(".download");
+    this.downloadBtn.addEventListener("click", this.downloadContent.bind(this));
 
-window.onload = function () {
-  document.querySelector("#preloader").classList.add("hide-preloader");
-};
+    window.onload = () => {
+      document.querySelector("#preloader").classList.add("hide-preloader");
+    };
 
-function randomQuote() {
-  quoteBtn.classList.add("loading");
-  quoteBtn.innerText = "Loading Quote...";
-  fetch("https://api.quotable.io/random")
-    .then((res) => res.json())
-    .then((result) => {
-      quoteText.innerText = result.content;
-      authorName.innerText = result.author;
-      quoteBtn.innerText = "Another One";
-      quoteBtn.classList.remove("loading");
-    })
-    .finally(() => {
-      if (localStorage.getItem("lastShownQuote")) {
-        let lastQuote = JSON.parse(localStorage.getItem("lastShownQuote"));
-        quoteText.innerText = lastQuote.content;
-        authorName.innerText = lastQuote.author;
-      }
-    });
-}
-
-soundBtn.addEventListener("click", () => {
-  if (speechSynthesis.speaking) {
-    speechSynthesis.cancel();
-  } else {
-    const utterance = new SpeechSynthesisUtterance(
-      `${quoteText.innerText} by ${authorName.innerText}`
-    );
-    // utterance.pitch = 1.5; // Change the pitch to make the voice sound lower
-    // utterance.rate = 1.5; // Change the rate to make the voice speak slower
-    speechSynthesis.speak(utterance);
+    this.quoteBtn.addEventListener("click", this.randomQuote.bind(this));
+    this.soundBtn.addEventListener("click", this.toggleSpeechSynthesis.bind(this));
+    this.copyBtn.addEventListener("click", this.copyQuote.bind(this));
+    this.twitterBtn.addEventListener("click", this.shareOnTwitter.bind(this));
+    this.printBtn.addEventListener("click", this.printContent.bind(this));
   }
-});
 
-copyBtn.addEventListener("click", () => {
-  navigator.clipboard.writeText(quoteText.innerText);
-});
+  randomQuote() {
+    this.quoteBtn.classList.add("loading");
+    this.quoteBtn.innerText = "Loading Quote...";
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((result) => {
+        this.quoteText.innerText = result.content;
+        this.authorName.innerText = result.author;
+        this.quoteBtn.innerText = "Another One";
+        this.quoteBtn.classList.remove("loading");
+      })
+      .finally(() => {
+        if (localStorage.getItem("lastShownQuote")) {
+          let lastQuote = JSON.parse(localStorage.getItem("lastShownQuote"));
+          this.quoteText.innerText = lastQuote.content;
+          this.authorName.innerText = lastQuote.author;
+        }
+      });
+  }
 
-twitterBtn.addEventListener("click", () => {
-  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
-  window.open(tweetUrl, "_blank");
-});
+  toggleSpeechSynthesis() {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    } else {
+      const utterance = new SpeechSynthesisUtterance(
+        `${this.quoteText.innerText} by ${this.authorName.innerText}`
+      );
+      speechSynthesis.speak(utterance);
+    }
+  }
 
-printBtn.addEventListener("click", () => {
-  window.print();
-});
+  downloadContent() {
+    const wrapper = document.querySelector('.wrapper');
+    const fileName = "quote-generator.png"; // You can change the file name if needed
 
-function download() {
-  // Create a canvas element with the same size as the wrapper
-  const canvas = document.createElement('canvas');
-  canvas.width = wrapper.offsetWidth;
-  canvas.height = wrapper.offsetHeight;
+    html2canvas(wrapper, { scale: 2 }).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = fileName;
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
+  }
 
-  // Get the canvas context and draw the wrapper content onto the canvas
-  const context = canvas.getContext('2d');
-  context.drawImage(wrapper, 0, 0);
+  copyQuote() {
+    const quote = this.quoteText.innerText;
+    navigator.clipboard.writeText(quote).then(() => {
+      const prompt = document.createElement("div");
+      prompt.classList.add("copy-prompt");
+      prompt.textContent = "Quote copied!";
+      document.body.appendChild(prompt);
+      setTimeout(() => {
+        prompt.remove();
+      }, 2000);
+    });
+  }
+  
+  shareOnTwitter() {
+    let tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(this.quoteText.innerText + " by " + this.authorName.innerText)}`;
+    window.open(tweetUrl, "_blank");
+  }
 
-  // Convert the canvas to a data URL
-  const dataURL = canvas.toDataURL();
-
-  // Create a temporary link element and set its href to the data URL
-  const link = document.createElement('a');
-  link.href = dataURL;
-
-  // Set the download attribute and filename of the link
-  link.download = 'wrapper.png';
-
-  // Add the link to the document and trigger a click event to start the download
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+  printContent() {
+    window.print();
+  }
 }
 
-quoteBtn.addEventListener("click", randomQuote);
+// Create an instance of the RandomQuoteGenerator class
+const quoteGenerator = new RandomQuoteGenerator();
---
+++ Random Quote Generator Project/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'css/styles.css',
+  'js/index.js',
+  // Add other files you want to cache here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
